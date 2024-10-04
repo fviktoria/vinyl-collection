3d6cbaca13fa3df15c91683bbbd86aace20e2e4a
Viktoria Ferstl <viktoria@functn.com>
2024-10-04 15:02:40 +0200

    feat(turborepo): fix wishlist type error

--- apps/frontend/src/util/wishlist.ts
+++ apps/frontend/src/util/wishlist.ts
@@ -1,56 +1,63 @@
-import { isUpstashWishlistItem } from '../types/guards/wishlist.guards';
-import { getRedisClient } from './redis-client';
+import { isUpstashWishlistItem } from "../types/guards/wishlist.guards";
+import { getRedisClient } from "./redis-client";
 
-import type { UpstashWishlistItemInterface } from '../types/wishlist.types';
+import type { UpstashWishlistItemInterface } from "../types/wishlist.types";
 
 export type SetReservedParams = {
-	id: string;
-	email?: string;
+  id: string;
+  email?: string;
 };
 
 export const getWishlistReserved = async (ids: string[]) => {
-	const redisClient = getRedisClient();
-	if (!redisClient) return;
+  const redisClient = getRedisClient();
+  if (!redisClient) return;
 
-	// will return: [ { reserved: true }, null ]
-	const values: (UpstashWishlistItemInterface | null)[] = await redisClient.mget(ids);
+  // will return: [ { reserved: true }, null ]
+  const values: (UpstashWishlistItemInterface | null)[] =
+    await redisClient.mget(ids);
 
-	// match values with ids (=keys)
-	const matchedValues: Record<string, UpstashWishlistItemInterface> = values.reduce(
-		(acc, value, index) => {
-			if (value !== null && isUpstashWishlistItem(value)) {
-				acc[ids[index]] = value;
-			}
-			return acc;
-		},
-		{} as Record<string, UpstashWishlistItemInterface>,
-	);
+  // match values with ids (=keys)
+  const matchedValues: Record<string, UpstashWishlistItemInterface> =
+    values.reduce(
+      (acc, value, index) => {
+        if (value !== null && isUpstashWishlistItem(value)) {
+          const idx = ids[index];
+          if (!idx) return acc;
+          acc[idx] = value;
+        }
+        return acc;
+      },
+      {} as Record<string, UpstashWishlistItemInterface>
+    );
 
-	return matchedValues;
+  return matchedValues;
 };
 
 export const setItemAsReserved = async ({ id, email }: SetReservedParams) => {
-	const redisClient = getRedisClient();
-	await redisClient?.set(id, { reserved: true, email });
+  const redisClient = getRedisClient();
+  await redisClient?.set(id, { reserved: true, email });
 };
 
-export const undoSetItemAsReserved = async ({ id, email }: SetReservedParams) => {
-	const redisClient = getRedisClient();
-	const item = await redisClient?.get<UpstashWishlistItemInterface>(id);
+export const undoSetItemAsReserved = async ({
+  id,
+  email,
+}: SetReservedParams) => {
+  const redisClient = getRedisClient();
+  const item = await redisClient?.get<UpstashWishlistItemInterface>(id);
 
-	if (!item) {
-		return { status: 'error', message: 'wishlist.error.item-not-found' };
-	}
+  if (!item) {
+    return { status: "error", message: "wishlist.error.item-not-found" };
+  }
 
-	// check email
-	if (!email || item.email !== email) {
-		return { status: 'error', message: 'wishlist.error.email-not-matching' };
-	}
+  // check email
+  if (!email || item.email !== email) {
+    return { status: "error", message: "wishlist.error.email-not-matching" };
+  }
 
-	try {
-		await redisClient?.del(id);
-		return { status: 'success' };
-	} catch {
-		return { status: 'error', message: 'wishlist.error.redis-error' };
-	}
+  try {
+    await redisClient?.del(id);
+    return { status: "success" };
+  } catch {
+    return { status: "error", message: "wishlist.error.redis-error" };
+  }
 };
